0e20868ca3ef533f3614066c92966c33df2f7982
ChrisW <christopher.woodthorpe@infineon.com>
2021-10-29 14:01:46 -0700

    devbranch changes

--- test1.c
+++ test1.c
@@ -1,7 +1,7 @@
 Test1.c
 
-Line 1 - GitHub Original
+Line 1 - Changed In DevBranch
 
 Line 2 - GitHub Original
 
-Line 2 - GitHub Original
+Line 2 - Changed In DevBranch
--- test2.c
+++ test2.c
@@ -1,6 +1,6 @@
 
 Line 1 - GitHub Original
 
-Line 2 - GitHub Original
+Line 2 - Changed In DevBranch
 
 Line 3 - GitHub Original
