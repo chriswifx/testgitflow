c87849dcd518a24a767798d825b04f8a211b3479
ChrisW <christopher.woodthorpe@infineon.com>
2021-11-01 16:05:32 -0700

    Modifications in Tortoisebranch

--- test1.c
+++ test1.c
@@ -1,14 +1,14 @@
 Test1.c
 
-Line 1 - Changed In devbb
+Line 1 - Changed In TortoiseBranch
 
 Line 2 - Changed in GitHub, devbb tests
 
-Line 3 - Changed In devv
+Line 3 - Changed In TortoiseBranch
 
-Line 4 - Added in DevBranch
+Line 4 - Modded in TortoiseBranch
 
-Line 5-  Added in devbb
+Line 5-  Modded in TortoiseBranch
 
 Line 6 - Added in GitHub, in parallel with devbb. Manually resolved merge conflict
 
--- test2.c
+++ test2.c
@@ -1,8 +1,8 @@
 
 Line 1 - GitHub Original, changed for devbb
 
-Line 2 - Changed In Devbb
+Line 2 - Changed In TortoiseBranch
 
 Line 3 - GitHub Original, changed for devbb
 
-Line 4 - Added in parallel with devbb
+Line 4 - Modded in TortoiseBranch
